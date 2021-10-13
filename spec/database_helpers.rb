52bd7f07edbc7fd550f3cca7369ce2d6616bb5fb
Lisa Mac <lisa@MacBook-Air-2.local>
2021-10-13 22:04:00 +0100

    Step 15 - Extracting a db setup object
    co-authored-by: Rob <rob.eagleheart@gmail.com>
    co-authored-by: Lisa <lisamac421@gmail.com>

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -2,6 +2,6 @@ require 'pg'
 
 def persisted_data(id:)
   connection = PG.connect(dbname: 'bookmark_manager_test')
-  result = connection.query("SELECT * FROM bookmarks WHERE id = #{id};")
+  result = connection.query("SELECT * FROM bookmarks WHERE id = $1;", [id])
   result.first
 end
